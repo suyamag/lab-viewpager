4a011c8bc9b8c2b2a716aeb3905e20f1e87d3f19
Grant Suyama <suyamag@uw.edu>
2018-10-24 23:05:37 -0700

    lab work

--- app/src/main/java/edu/uw/viewpager/MainActivity.kt
+++ app/src/main/java/edu/uw/viewpager/MainActivity.kt
@@ -1,16 +1,61 @@
 package edu.uw.viewpager
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
 import android.support.v4.app.FragmentTransaction
+import android.support.v4.view.PagerAdapter
+import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.EditText
 
-class MainActivity : AppCompatActivity(), MovieListFragment.OnMovieSelectedListener {
+class MainActivity : AppCompatActivity(), MovieListFragment.OnMovieSelectedListener, SearchFragment.OnSearchListener {
+
+    private var searchFragment: SearchFragment? = null
+    private var movieListFragment: MovieListFragment? = null
+    private var detailsFragment: DetailFragment? = null
+    private var viewPager: ViewPager? = null
+    private var pagerAdapter: PagerAdapter? = null
+
+    private inner class MoviePagerAdapter(fManager: FragmentManager) : FragmentStatePagerAdapter(fManager) {
+        override fun getItem(position: Int): Fragment? {
+            if (position == 0) {
+                return searchFragment
+            } else if (position == 1) {
+                return movieListFragment
+            } else if (position == 2) {
+                return detailsFragment
+            } else {
+                return null
+            }
+        }
+
+        override fun getCount(): Int {
+            if (movieListFragment == null) {
+                return 1
+            } else if (detailsFragment == null) {
+                return 2
+            } else {
+                return 3
+            }
+        }
+
+        override fun getItemPosition(`object`: Any?): Int {
+            return PagerAdapter.POSITION_NONE
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        searchFragment = SearchFragment.newInstance()
+
+        viewPager = findViewById(R.id.view_pager)
+        pagerAdapter = MoviePagerAdapter(supportFragmentManager)
+        viewPager!!.adapter = pagerAdapter
     }
 
     //respond to search button clicking
@@ -21,7 +66,7 @@ class MainActivity : AppCompatActivity(), MovieListFragment.OnMovieSelectedListe
         val fragment = MovieListFragment.newInstance(searchTerm)
 
         val ft = supportFragmentManager.beginTransaction()
-        ft.replace(R.id.container, fragment, MOVIE_LIST_FRAGMENT_TAG)
+        ft.replace(R.id.view_pager, fragment, MOVIE_LIST_FRAGMENT_TAG)
         ft.addToBackStack(null)
         ft.commit()
     }
@@ -30,10 +75,18 @@ class MainActivity : AppCompatActivity(), MovieListFragment.OnMovieSelectedListe
         val fragment = DetailFragment.newInstance(movie)
 
         val ft = supportFragmentManager.beginTransaction()
-        ft.replace(R.id.container, fragment, MOVIE_DETAIL_FRAGMENT_TAG)
+        ft.replace(R.id.view_pager, fragment, MOVIE_DETAIL_FRAGMENT_TAG)
         ft.addToBackStack(null) //remember for the back button
         ft.commit()
     }
+
+    override fun onSearchSubmitted(searchTerm: String) {
+        movieListFragment = MovieListFragment.newInstance(searchTerm)
+        pagerAdapter!!.notifyDataSetChanged()
+        viewPager!!.currentItem = 1
+    }
+
+
 
     companion object {
 
